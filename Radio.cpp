b89a6bd53447fda13a968fea9b7792855e8b42b2
tolsoty92 <34340970+tolsoty92@users.noreply.github.com>
2017-12-08 02:20:33 +0300

    Add files via upload

--- Radio.cpp
+++ Radio.cpp
@@ -5,36 +5,39 @@
 
 TrR::TrR(){}
 
-void TrR::begin(int pipe_num)
+void TrR::begin()
 	/* =========================================
 	Функция, предназначена для инициализации
 	работы радиомодуля.
-	В качестве аргумента принимает номер трубы (0-5).	
 	===========================================*/
 	
 {
-  radio = new RF24(ce, csn);  					// Номера пинов, подключаемых к ce и csn выходам радиомодуля
+  radio = new RF24(9, 10);  					// Номера пинов, подключаемых к ce и csn выходам радиомодуля
   radio->begin();                               //  Начало работы      
-  radio->setChannel(0x55);    					// Номер канала связи                           
+  radio->setChannel(0x50);    					// Номер канала связи                           
   radio->setDataRate     (RF24_1MBPS);   		// Скорость передачи данных                
   radio->setPALevel      (RF24_PA_HIGH);    	// Мощность работы радиомодуля            
-  radio->openReadingPipe (0, pipes[pipe_num]);	// Открытие трубы
+  	// Открытие трубы
 }
 
-void TrR::startListening()
+void TrR::startListening(int pipe_num)
 	/* =========================================
 	Функция запускает решим приема данных.	
+  В качестве аргумента принимает номер трубы (0-5).	
 	===========================================*/
 	
 {
+  radio->openReadingPipe (0, pipes[pipe_num]);
   radio->startListening();
 }
 
-void TrR::stopListening()
+void TrR::startWriting(int pipe_num)
 	/* =========================================
 	Функция останавливает решим приема данных.	
+  В качестве аргумента принимает номер трубы (0-5).	
 	===========================================*/
 {
+  radio->openWritingPipe (pipes[pipe_num]);
   radio->stopListening();
 }
 
@@ -55,9 +58,9 @@ void TrR::receive(int &x1, int &x2, int &x3, int &x4, int &x5, int &x6)
   x3 = msg[2];
   x4 = msg[3];
   x5 = msg[4];
-  x6 = msg[5]}
+  x6 = msg[5];}
   else{
-  Serial.print('Radio is not available\n');}
+  Serial.print("Radio is not available\n");}
 }
 
 void TrR::transmitte(int x1, int x2, int x3, int x4, int x5, int x6)
--- Radio.h
+++ Radio.h
@@ -9,14 +9,14 @@ class TrR
 {
 	public:
 		const int ce = 9;
-		const int cn = 10;
+		const int csn = 10;
 		uint64_t pipes[10] = {0xF0F0F0F000, 0xF0F0F0F011, 0xF0F0F0F022, 0xF0F0F0F033, 0xF0F0F0F044, 0xF0F0F0F055};
 		RF24 *radio;
 		int msg[10];
 		TrR();
-		void begin(int pipe_num);    
-		void startListening();
-		void stopListening();  
+		void begin();    
+		void startListening(int pipe_num);
+		void startWriting(int pipe_num);  
 		void receive(int &x1, int &x2, int &x3, int &x4, int &x5, int &x6);
 		void transmitte(int x1, int x2, int x3, int x4, int x5, int x6);
 };
