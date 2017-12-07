77d603cabbaf6a3e5765f67c239ef8e2bf479f43
tolsoty92 <34340970+tolsoty92@users.noreply.github.com>
2017-12-07 15:45:22 +0300

    Add files via upload

---
+++ Radio.h
@@ -0,0 +1,23 @@
+#ifndef Radio_h
+#define Radio_h
+
+#include "Arduino.h"
+#include <SPI.h>             
+#include <nRF24L01.h>        
+#include <RF24.h> 
+class TrR
+{
+	public:
+		const int ce = 9;
+		const int cn = 10;
+		uint64_t pipes[10] = {0xF0F0F0F000, 0xF0F0F0F011, 0xF0F0F0F022, 0xF0F0F0F033, 0xF0F0F0F044, 0xF0F0F0F055};
+		RF24 *radio;
+		int msg[10];
+		TrR();
+		void begin(int pipe_num);    
+		void startListening();
+		void stopListening();  
+		void receive(int &x1, int &x2, int &x3, int &x4, int &x5, int &x6);
+		void transmitte(int x1, int x2, int x3, int x4, int x5, int x6);
+};
+#endif
